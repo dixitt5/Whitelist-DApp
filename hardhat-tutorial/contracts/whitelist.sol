9021fa147b1e129931da2b5f25fc9f0d22f3a934
dixitt5 <tilajidixit@gmail.com>
2022-10-30 00:20:28 +0530

    initial commits

---
+++ hardhat-tutorial/contracts/whitelist.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract whitelist {
+    uint8 public maxWhiteListedAddresses;
+    uint8 public numAddressesWhiteListed;
+
+    mapping(address => bool) public whiteListedAddresses;
+
+    constructor(uint8 _maxWhiteListedAddresses) {
+        maxWhiteListedAddresses = _maxWhiteListedAddresses;
+    }
+
+    function addAddressaToWhiteList() public {
+        require(
+            !whiteListedAddresses[msg.sender],
+            "Sender already in whitelist"
+        );
+        require(
+            numAddressesWhiteListed < maxWhiteListedAddresses,
+            "max limit reached"
+        );
+        whiteListedAddresses[msg.sender] = true;
+        numAddressesWhiteListed += 1;
+    }
+}
